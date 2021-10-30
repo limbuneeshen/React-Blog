9266975993821b051b434b33d1db4b82c9f0b176
limbuneeshen <limbuneeshen@gmail.com>
2021-10-30 17:24:28 +0545

    Blog with unsplash api

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostList from './PostList';
+
+const App = () =>{
+    return(
+    <div>
+        <div className="ui container"><PostList /></div>
+    </div>
+        );
+};
+
+export default App;
